86f8aa13f11f556480fd6e237dac09ce6e10443c
super-nova-beam <101120258+super-nova-beam@users.noreply.github.com>
2022-08-18 04:17:23 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,4 +1,3 @@
-
 //ADD YOUR FIREBASE LINKS HERE
 const firebaseConfig = {
       apiKey: "AIzaSyCLgdRgjVBHUkuko1IcTKO27EKjmXdVKhA",
@@ -9,14 +8,44 @@ const firebaseConfig = {
       messagingSenderId: "544606793998",
       appId: "1:544606793998:web:be727490dcbabdda38a75f",
       measurementId: "G-YGLTSPN5Q5"
-    };
-    
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
+};
 
-      //End code
-      });});}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+user_name = localStorage.getItem("user_name");
+
+function addRoom() {
+      room_name = document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose: "adding Room Name",
+      });
+      localStorage.setItem("room_name", room_name);
+      window.location = "Kwitter_page.html";
+}
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = "";
+            snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+                  //Start code
+                  console.log("room name" + Room_names);
+                  row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+                  document.getElementById("output").innerHTML += row;
+                  //End code
+            });
+      });
+}
 getData();
+function redirectToRoomName(name){
+      console.log(name);
+      localStorage.setItem("room_name", room_name);
+      window.location = "Kwitter_page.html";
+}
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "index.html";
+}
