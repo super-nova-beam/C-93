0cf8027ff976c484926ac0b775495719c3c46883
super-nova-beam <101120258+super-nova-beam@users.noreply.github.com>
2022-08-24 04:11:06 -0700

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -36,10 +36,32 @@ function getData() {
                         firebase_message_id = childKey;
                         message_data = childData;
                         //Start code
-
+                        console.log(firebase_message_id);
+                        console.log(message_data);
+                        name = message_data["name"];
+                        message = message_data["message"];
+                        like = message_data["like"];
+                        name_with_tag = "<h4> " + name + "<img class='user_tick' src='tick.png'>";
+                        message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+                        like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'>";
+                        span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: " + like + "</span></button><hr>";
+                        row = name_with_tag+message_with_tag+like_button+span_with_tag;
+                        document.getElementById("output").innerHTML+=row;
                         //End code
                   }
             });
+      });
+}
+getData();
+function updateLike(message_id) {
+      console.log("clicked on like button - " + message_id);
+      button_id = message_id;
+      likes = document.getElementById(button_id).value;
+      updated_likes = Number(likes) + 1;
+      console.log(updated_likes);
+
+      firebase.database().ref(room_name).child(message_id).update({
+            like: updated_likes
       });
 }
 
